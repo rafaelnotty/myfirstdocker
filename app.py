70baef07dc9c7f813d5fa81d730f66e3ee1cc754
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-10 20:17:00 +0000

    pruebas incluy pandas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import subprocess
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,6 +12,14 @@ def index():
 def ejecutar():
     output = subprocess.check_output(['python', 'codigo1.py'])
     return output
+
+# Función para la página de la tabla
+@app.route('/tabla')
+def tabla():
+    data = {'fecha': ['5/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]}
+    df = pd.DataFrame(data)
+    table_html = df.to_html()
+    return render_template('tabla.html', table=table_html)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- codigo1.py
+++ codigo1.py
@@ -1,10 +1,11 @@
+from flask import render_template
 import numpy as np
 import pandas as pd
 """ 
 Prueba Docstring
     """
 arr = np.arange(1,5)
-df = pd.DataFrame({'fecha': ['5/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]})
-print(str(df.columns.tolist()))
-print(str(list(arr)))
-df.to_html()
+df = pd.DataFrame({'fecha': ['50/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]})
+#print(str(df.columns.tolist()))
+#print(str(list(arr)))
+render_template(df.to_html())
