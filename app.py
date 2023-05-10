f52f05248ab37c971998ca9e3f5c97f3e58909a0
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-10 20:34:28 +0000

    pruebas incluy pandas

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    data = {'fecha': ['5/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]}
+    df = pd.DataFrame(data)
+    table_html = df.to_html()
+    return render_template('index.html', table=table_html)
 
 @app.route('/ejecutar', methods=['POST'])
 def ejecutar():
@@ -16,10 +20,9 @@ def ejecutar():
 # Función para la página de la tabla
 @app.route('/tabla')
 def tabla():
-    data = {'fecha': ['02/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]}
+    data = {'fecha': ['5/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]}
     df = pd.DataFrame(data)
-    table = df.to_html()
-    return render_template('tabla.html', table=table)
+    table_html = df.to_html()
+    return render_template('tabla.html', table=table_html)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
