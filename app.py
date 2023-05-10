2a70d1ffe8b9844506345baa67d6cadcc28a8c8a
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-10 19:36:33 +0000

    tratando de corregir errs

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 import subprocess
-import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
 
--- codigo1.py
+++ codigo1.py
@@ -1,7 +1,1 @@
 print("Hola Mundo_")
-
-df = pd.DataFrame()
-print(len(df))
-
-arr = np.arange(1, 5)
-print(str(list(arr)))
