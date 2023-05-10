1049e54aad7c3cc4d1523a7cabf11dc3c7c641ed
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-10 20:23:14 +0000

    pruebas incluy pandas

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def ejecutar():
 # Función para la página de la tabla
 @app.route('/tabla')
 def tabla():
-    data = {'fecha': ['5/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]}
+    data = {'fecha': ['02/22/20', '5/23/20', '5/24/20'], 'valor1': [10, 20, 30], 'valor2': [15, 25, 35]}
     df = pd.DataFrame(data)
-    table_html = df.to_html()
-    return render_template('tabla.html', table=table_html)
+    table = df.to_html()
+    return render_template('tabla.html', table=table)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
