1cd2b49b79bf0b5e05ce5570aeb2c7ab38d6e524
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-10 19:21:12 +0000

    corrigiendo el lugar de los import

--- codigo1.py
+++ codigo1.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import numpy as np
+
 print("Hola Mundo_")
 
 df = pd.DataFrame()
 print(len(df))
-arr = np.arange(1,10)
-print(arr)
+
+arr = np.arange(1, 5)
+print(str(list(arr)))
